72b8498e75ded572269d992ee5c0b8f983e66610
Ethan Landrum <landrum.ethan.2312@gmail.com>
2022-02-14 15:13:48 -0600

    Slinhshot
    Created and coded the slingshot.

---
+++ Mission Demolition/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+    }
+        
+    void OnMouseExit()
+    {
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
